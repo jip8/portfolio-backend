1e08bea436f2e11e348fc4274bc1106999682051
Jip <pedro.lm.silva@hotmail.com>
2025-10-04 17:57:06 -0300

    tabelas banco de dados definidos

--- internal/api/experiences/repositories/create.go
+++ internal/api/experiences/repositories/create.go
@@ -2,7 +2,6 @@ package repositories
 
 import (
 	"context"
-	"time"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/jip/portfolio-backend/internal/entity"
@@ -24,24 +23,19 @@ func NewCreateRepository(config *entity.Config, redisClient *redis.Client, db *s
 }
 
 func (r *CreateRepository) Execute(ctx context.Context, req entity.ExperienceFlat) (*int, error) {
-	initialDate, err := time.Parse("2006-01-02", *req.InitialDate)
-	if err != nil {
-		return nil, err
-	}
-
-	endDate, err := time.Parse("2006-01-02", *req.EndDate)
-	if err != nil {
-		return nil, err
-	}
-
 	query := `
-		INSERT INTO experiences (title, function, description, initial_date, end_date)
-		VALUES ($1, $2, $3, $4, $5)
+		INSERT INTO experiences (title, "function", description, initial_date, end_date)
+		VALUES (:title, :function, :description, :initial_date, :end_date)
 		RETURNING id
 	`
 
+	namedQuery, args, err := r.db.BindNamed(query, req)
+	if err != nil {
+		return nil, err
+	}
+
 	var newID int
-	err = r.db.QueryRowContext(ctx, query, req.Title, *req.Function, *req.Description, initialDate, endDate).Scan(&newID)
+	err = r.db.QueryRowContext(ctx, namedQuery, args...).Scan(&newID)
 	if err != nil {
 		return nil, err
 	}
